2057c394fbde4d34723d0e3aa07a490f32534e24
apparition <aaron@onefatgiraffe.com>
2018-03-06 14:46:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Add Easy Category Icon to WP REST API
+
+This plugin adds the easy category icons parameter to API categories.
+
+## Usage
+
+1. Install [Easy Category Icons](https://wordpress.org/plugins/templatic-categoryicons).
+2. Call `http://<WP-SITE>/wp-json/wp/v2/categories`
---
+++ plugin.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: Add Easy Category Icon to WP REST API
+ * Description: This plugin adds the easy category icons parameter to API categories.
+ * Author: Clintal
+ * Author URI: https://www.clintal.com
+ * Version: 0.1
+ * License: LGPL
+ **/
+
+add_action( 'rest_api_init', 'slug_register_easycategory' );
+function slug_register_easycategory() {
+    register_rest_field( 'category',
+        'easy_category_icon',
+        array(
+            'get_callback'    => 'slug_get_easycategory_icon_name',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+
+function slug_get_easycategory_icon_name( $object, $field_name, $request ) {
+    $cat = get_category_by_slug($object['slug']);
+    preg_match('/fa-([a-z]*)[.\S]/', $cat->term_font_icon, $matches);
+    return $matches[1];
+}
